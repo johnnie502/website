7bd59062435eaaccbd0959798efc6403df3a67bc
iVanilla <precomx@live.com>
2017-03-04 12:02:01 +0800

    Add mails and notifications

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Recca0120\Upload\UploadManager;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
     public function uploadAvatar(Request $request)
     {
-        $file = $request->file('avatar');
-        $manager = UploadManager::getInstance();
-        $upload = $manager->upload($file);
-        $upload->save();
-        return $upload;
+    	// Update avatar.
+         $path = $request->file('avatar')->storeAs('avatars/', $request->user()->id);
+        return $path;
     }
 }

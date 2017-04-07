dd49a1ca29c71b713a415eee9b80780e317ac5bf
iVanilla <precomx@live.com>
2017-04-08 02:06:50 +0800

    Add piwik code to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,22 @@ ______                            _              _                              
         // Flash
         $('#flash-overlay-modal').modal();
         $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+        // Piwik
+        <script type="text/javascript">
+        var _paq = _paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//mpcblab.com/piwik/";
+             _paq.push(['setTrackerUrl', u+'piwik.php']);
+             _paq.push(['setSiteId', '2']);
+             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+             g.type='text/javascript';
+             g.async=true; g.defer=true;
+             g.src=u+'piwik.js';
+
+s.parentNode.insertBefore(g,s); })();
     </script>
 </head>
 
